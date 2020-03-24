8f9f0f0c295b53d182305350319118addec44a08
Murad Cholukov <mcholukov@welyons.local>
2020-03-24 14:36:06 -0700

    deleted password from public

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Neb1raska",
+    password: "my secret password goes here",
     database: "bamazon"
 });
 
